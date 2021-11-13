1b780676d1f551594dba929ed2ab459bfc88ced0
user94729 <user94729@omegazero.org>
2021-11-13 20:59:53 +0100

    Incremented version number

--- src/xyz/user94729/mc/launcher/MCLauncher.java
+++ src/xyz/user94729/mc/launcher/MCLauncher.java
@@ -37,7 +37,7 @@ import xyz.user94729.mc.launcher.session.PlayerSession;
 public final class MCLauncher {
 
 	public static final String BRAND = "U9 MCLauncher";
-	public static final String VERSION = "2.1";
+	public static final String VERSION = "2.2";
 
 	private static final Logger logger = Logger.create();
 
